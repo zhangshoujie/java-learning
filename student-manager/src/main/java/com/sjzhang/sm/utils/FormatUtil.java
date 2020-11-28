989eed5f63db7911c0b4f1f96372ea0284caea97
zhangshoujie <1753141460@qq.com>
2020-11-28 19:14:09 +0800

    submit

--- student-manager/src/main/java/com/sjzhang/sm/utils/FormatUtil.java
+++ student-manager/src/main/java/com/sjzhang/sm/utils/FormatUtil.java
@@ -1,8 +1,27 @@
-package com.sjzhang.sm.utils;/**
-  *@ClassName FormatUtil
-  *@Description TODO
-  *@Author 张守杰
-  *@Date 2020/11/26
-**/
+package com.sjzhang.sm.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @ClassName FormatUtil
+ * @Description TODO
+ * @Author 张守杰
+ * @Date 2020/11/26
+ **/
 public class FormatUtil {
+    public static String formatDate(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date);
+    }
+
+    public static String formatGender(int gender) {
+        if (gender == 0) {
+            return "保密";
+        } else if (gender == 1) {
+            return "男";
+        } else {
+            return "女";
+        }
+    }
 }
